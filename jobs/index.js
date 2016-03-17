671019b2fe276511fe6a6714af239e78f0be73d3
Henry Muru Paenga <meringu@gmail.com>
2016-03-17 23:18:14 +1300

    Jobs for the new flat

--- jobs/index.js
+++ jobs/index.js
@@ -1,57 +1,39 @@
-var visible_job;
-var week;
-
 $(document).ready(function () {
-  start = new Date(2015, 0, 26, 0, 0, 0, 0);
-  week = weeks_since(start);
-  visible_job = get_job(0);
-  visible_job.addClass('in');
+  var dates = [];
 
-  $('#flat-cop-' + (mod(-week + 0, 3))).html('Mopping - coordinate with sweeper.');
-  $('#flat-cop-' + (mod(-week + 1, 3))).html('Clean fridge.');
-  $('#flat-cop-' + (mod(-week + 2, 3))).html('Clean microwave.');
-
-  if (week % 2 == 0) {
-    $('#gardening-0').html('This week bags.');
-    $('#gardening-1').html('Next week bin.');
+  for (var i = 0; i < 3; i++) {
+    var date = Date.today().add(-8 + i * 7).days().next().sunday();
+    dates.push(date.getDate() + '/' + (date.getMonth() + 1));
   }
-  else {
-    $('#gardening-0').html('This week bin.');
-    $('#gardening-1').html('Next week bags.');
+
+  var job_table = $('#job-table');
+
+  job_table.append($('<thead>')
+    .append($('<tr>')
+      .append($('<th>').append('Flatmate'))
+      .append($('<th>').append('Last Week ' + dates[0]))
+      .append($('<th>').append('This Week ' + dates[1]))
+      .append($('<th>').append('Next Week ' + dates[2]))
+    )
+  )
+
+  var flatmates = ['Martin', 'Aliscia', 'Ryan', 'Joel', 'Henry', 'Chlo&eacute;'];
+
+  var jobs = [
+    'Bathroom',
+    'Dishes + Surfaces + Microwave',
+    'Rear Shower',
+    'Rubbish Bins',
+    'Kitchen + Bathroom Floors',
+    'Vacuum'
+  ]
+
+  for (var i = 0; i < flatmates.length; i++) {
+    job_table.append($('<tr>')
+      .append($('<td>').append(flatmates[i]))
+      .append($('<td>').append(jobs[i]))
+      .append($('<td>').append(jobs[(i + jobs.length - 1) % jobs.length]))
+      .append($('<td>').append(jobs[(i + jobs.length - 2) % jobs.length]))
+    )
   }
 });
-
-function weeks_since(date) {
-  var seconds = Math.floor((new Date() - date) / 1000);
-  return Math.floor(seconds / 86400 / 7);
-}
-
-function dropdown_changed() {
-  var value = $('#dropdown').val();
-  visible_job.on('hidden.bs.collapse', function () {
-    show_next();
-  });
-  visible_job.collapse('hide');
-  visible_job = get_job(value);
-}
-
-function get_job(value) {
-  if (value == 0)
-    return $('#job-everyone');
-  if (value == (week + 0) % 4 + 1)
-    return $('#job-bathroom');
-  if (value == (week + 1) % 4 + 1)
-    return $('#job-cop');
-  if (value == (week + 2) % 4 + 1)
-    return $('#job-gardening');
-  if (value == (week + 3) % 4 + 1)
-    return $('#job-sweeping');
-}
-
-function show_next() {
-  visible_job.collapse('show');
-}
-
-function mod(a, b) {
-  return ((a%b)+b)%b;
-}
